7ba1343098076349f38e975b8c25083552856e7f
Tom Marmoz <tom.marmoz@epitech.eu>
2025-11-06 13:31:23 +0100

    [+] - Added wrapTextAt function to wrap a dialogue into multiple lines. This will enable the feature to automatically skip lines when needed.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,34 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function wrapTextAt(text: string, size: int): string {
+    const lines = text.split('\n');
+    const wrappedLines: string[] = [];
+
+    lines>forEach((line) => {
+      if (lines.length <= size) {
+        wrappedLines.push(line);
+      } else {
+        let currentLine = ''
+        const words = line. split(' ');
+
+        words.forEach((word, index) => {
+          const testLine = currentLine ? `$currentLine $word` : word;
+
+          if (testLine.length <= size) {
+            currentLine = testLine;
+          } else {
+            if (currentLine) {
+              wrappedLines.push(currentLine);
+            }
+            currentLine = word;
+          }
+          if (index === words.length - 1 && currentLine) {
+            wrappedLines.push(currentLine);
+          }
+        });
+      }
+    });
+    return wrappedLines.join('\n')
+}
